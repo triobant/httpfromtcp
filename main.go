cc9c619d081c055710ae284024f8df239b393b0f
Brian Ott <bott@b1-systems.de>
2025-03-17 19:15:52 +0100

    update: reading 8 bytes from file and printing until eof

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+    "errors"
+    "fmt"
+    "io"
+    "log"
+    "os"
+)
+
+const inputFilePath = "messages.txt"
 
 func main() {
-    fmt.Printf("I hope I get the job!")
+    file, err := os.Open(inputFilePath)
+    if err != nil {
+        log.Fatalf("could not open %s: %s\n", inputFilePath, err)
+    }
+
+    fmt.Printf("Reading data from %s\n", inputFilePath)
+    fmt.Println("=====================================")
+
+    for {
+        buffer := make([]byte, 8, 8)
+        n, err := file.Read(buffer)
+        if err != nil {
+            if errors.Is(err, io.EOF) {
+                break
+            }
+            fmt.Printf("error: %s\n", err.Error())
+            break
+        }
+        str := string(buffer[:n])
+        fmt.Printf("read: %s\n", str)
+    }
 }
