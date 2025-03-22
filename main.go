e86de7d9cfebafdb2ae7fb6e935f449afab6b2b5
Brian Ott <bott@b1-systems.de>
2025-03-22 20:19:36 +0100

    update: refactored code to a reusable function that reads lines from a tcp connection

--- main.go
+++ main.go
@@ -20,30 +20,41 @@ func main() {
     fmt.Printf("Reading data from %s\n", inputFilePath)
     fmt.Println("=====================================")
 
-    currentLineContents := ""
-    for {
-        buffer := make([]byte, 8, 8)
-        n, err := file.Read(buffer)
-        if err != nil {
-            if currentLineContents != "" {
-                fmt.Printf("read: %s\n", currentLineContents)
-                currentLineContents = ""
-            }
-            if errors.Is(err, io.EOF) {
-                break
-            }
-            fmt.Printf("error: %s\n", err.Error())
-            break
-        }
-        str := string(buffer[:n])
-        parts := strings.Split(str, "\n")
-        for i := 0; i < len(parts) - 1; i++ {
-            fmt.Printf("read: %s%s\n", currentLineContents, parts[i])
-            currentLineContents = ""
-        }
-        currentLineContents += parts[len(parts) - 1]
+    linesChan := getLinesChannel(file)
+
+    for line := range linesChan {
+        fmt.Printf("read: %s\n", line)
     }
 }
 
 func getLinesChannel(f io.ReadCloser) <-chan string {
+    lines := make(chan string)
+    go func() {
+        defer f.Close()
+        defer close(lines)
+        currentLineContents := ""
+        for {
+            buffer := make([]byte, 8, 8)
+            n, err := f.Read(buffer)
+            if err != nil {
+                if currentLineContents != "" {
+                    lines <- currentLineContents
+                }
+                if errors.Is(err, io.EOF) {
+                    break
+                }
+                fmt.Printf("error: %s\n", err.Error())
+                break
+            }
+            str := string(buffer[:n])
+            parts := strings.Split(str, "\n")
+            for i := 0; i < len(parts) - 1; i++ {
+                lines <- fmt.Sprintf("%s%s", currentLineContents, parts[i])
+                currentLineContents = ""
+            }
+            currentLineContents += parts[len(parts) - 1]
+        }
+    }()
+
+    return lines
 }
