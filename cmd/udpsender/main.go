829a8c18cb929426236a9ecab6f91adbbe4a5ed0
Brian Ott <bott@b1-systems.de>
2025-07-23 20:27:46 +0200

    update: reading from stdin and writing msg to udp connection

--- cmd/udpsender/main.go
+++ cmd/udpsender/main.go
@@ -20,4 +20,18 @@ func main() {
         log.Fatalf("error opening udp connection: %s\n", err.Error())
     }
     defer conn.Close()
+
+    reader := bufio.NewReader(os.Stdin)
+    for {
+        fmt.Print(">")
+	stdin, err := reader.ReadString('\n')
+	if err != nil {
+	    log.Fatalf("error reading input string: %s", err.Error())
+	}
+
+	_, err = conn.Write([]byte(stdin))
+	if err != nil {
+	    log.Fatalf("error writing input to udp connection: %s", err.Error())
+	}
+    }
 }
