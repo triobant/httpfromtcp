cc98c7c936683dde064979ef378f92355a1ead4c
Brian Ott <bott@b1-systems.de>
2025-07-31 19:33:29 +0200

    update: test for invalid method

--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -36,4 +36,8 @@ func TestRequestLineParse(t *testing.T) {
     // Test: Invalid number of parts in request line
     _, err = RequestFromReader(strings.NewReader("/coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
     require.Error(t, err)
+
+    // Test: Invalid method (out of order) Request line
+    _, err = RequestFromReader(strings.NewReader("/coffee POST HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+    require.Error(t, err)
 }
