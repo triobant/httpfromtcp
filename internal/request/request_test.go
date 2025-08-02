111a90e11faa35810d768d4d6d41b0d51a5adc23
Brian Ott <bott@b1-systems.de>
2025-08-02 23:10:48 +0200

    update: use chunkReader type

--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -10,6 +10,10 @@ import (
 
 func TestRequestLineParse(t *testing.T) {
     // Test: Good GET Request line
+    reader := &chunkReader{
+        data: 		 "GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+	numBytesPerRead: 3,
+    }
     r, err := RequestFromReader(strings.NewReader("GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
     require.NoError(t, err)
     require.NotNil(t, r)
@@ -18,6 +22,10 @@ func TestRequestLineParse(t *testing.T) {
     assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
 
     // Test: Good GET Request line with path
+    reader := &chunkReader{
+        data: 		 "GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+	numBytesPerRead: 3,
+    }
     r, err = RequestFromReader(strings.NewReader("GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
     require.NoError(t, err)
     require.NotNil(t, r)
