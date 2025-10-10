2ec5e79d55001e72399a8809f6376459a5b2dc97
Brian Ott <bott@b1-systems.de>
2025-10-10 18:26:52 +0200

    update: removed comments

--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -76,8 +76,6 @@ type chunkReader struct {
 	pos             int
 }
 
-// Read reads up to len(p) or numBytesPerRead bytes from the string per call
-// its useful for simulating reading a variable number of bytes per chunk from a network connection
 func (cr *chunkReader) Read(p []byte) (n int, err error) {
 	if cr.pos >= len(cr.data) {
 		return 0, io.EOF
