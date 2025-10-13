588d4fa5d6c719b1b956cc89546416909471b73b
Brian Ott <bott@b1-systems.de>
2025-10-13 22:48:02 +0200

    update: delete commented lines and rewrote returned error

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -22,52 +22,8 @@ func main() {
     for {
         conn, err := listener.Accept()
 	if err != nil {
-	    log.Fatalf("failed to accept connection: %s\n", err)
-	    continue
+	    log.Fatalf("error: %s\n", err.Error())
 	}
-
-	fmt.Printf("Reading data from %s\n", conn)
-	fmt.Println("=====================================")
-
-//	linesChan := getLinesChannel(conn)
-	linesChan := RequestFromReader(conn)
-
-	for line := range linesChan {
-	    fmt.Printf("read: %s\n", line)
-	}
+	fmt.Println("Accepted connection from", conn.RemoteAddr())
     }
 }
-
-/**
-func getLinesChannel(f io.ReadCloser) <-chan string {
-    lines := make(chan string)
-    go func() {
-        defer f.Close()
-        defer close(lines)
-        currentLineContents := ""
-        for {
-            buffer := make([]byte, 8, 8)
-            n, err := f.Read(buffer)
-            if err != nil {
-                if currentLineContents != "" {
-                    lines <- currentLineContents
-                }
-                if errors.Is(err, io.EOF) {
-                    break
-                }
-                fmt.Printf("error: %s\n", err.Error())
-                break
-            }
-            str := string(buffer[:n])
-            parts := strings.Split(str, "\n")
-            for i := 0; i < len(parts) - 1; i++ {
-                lines <- fmt.Sprintf("%s%s", currentLineContents, parts[i])
-                currentLineContents = ""
-            }
-            currentLineContents += parts[len(parts) - 1]
-        }
-    }()
-
-    return lines
-}
-**/
