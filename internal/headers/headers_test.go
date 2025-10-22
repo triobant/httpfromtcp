2f334784d9bccde0a82cd3f28529d40fe6b8f93b
Brian Ott <bott@b1-systems.de>
2025-10-22 06:58:17 +0200

    update: test for valid single header

--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -9,4 +9,13 @@ import (
 )
 
 func TestHeadersParse(t *testing.T) {
+    // Test: Valid single header
+    headers := NewHeaders()
+    data := []byte("Host: localhost:42069\r\n\r\n")
+    n, done, err := headers.Parse(data)
+    require.NoError(t, err)
+    require.NotNil(t, headers)
+    assert.Equal(t, "localhost:42069", headers["Host"])
+    assert.Equal(t, 23, n)
+    assert.False(t, done)
 }
