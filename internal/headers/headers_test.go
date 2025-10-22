220c8bf9905043396d69c4340db7a02338dfe5c9
Brian Ott <bott@b1-systems.de>
2025-10-22 06:58:36 +0200

    update: test for invalid spacing header

--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -18,4 +18,12 @@ func TestHeadersParse(t *testing.T) {
     assert.Equal(t, "localhost:42069", headers["Host"])
     assert.Equal(t, 23, n)
     assert.False(t, done)
+
+    // Test: Invalid spacing header
+    headers = NewHeaders()
+    data = []byte("       Host : localhost:42069       \r\n\r\n")
+    n, done, err = headers.Parse(data)
+    require.Error(t, err)
+    assert.Equal(t, 0, n)
+    assert.False(t, done)
 }
