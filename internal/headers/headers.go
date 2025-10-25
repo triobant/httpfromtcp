54b2e787e43d1c524a2519ff7bf86cd66968079b
Brian Ott <bott@b1-systems.de>
2025-10-25 23:05:04 +0200

    update: index where crlf first appears if found

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -9,6 +9,7 @@ const crlf = "\r\n"
 type Headers map[string]string
 
 func (h Headers) Parse(data []byte) (n int, done bool, err error) {
+    idx := bytes.Index(data, []byte(crlf))
     h[key] = value
     return n, done, err
 }
