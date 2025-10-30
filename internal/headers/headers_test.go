b1d97f37a3342531fc8032294f589624b1e83364
Brian Ott <bott@b1-systems.de>
2025-10-30 17:03:37 +0100

    update: test for valid single header with extra whitespace

--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -26,4 +26,14 @@ func TestHeadersParse(t *testing.T) {
     require.Error(t, err)
     assert.Equal(t, 0, n)
     assert.False(t, done)
+
+    // Test: Valid single header with extra whitespace
+    headers = NewHeaders()
+    data = []byte("Host: localhost   :   42069\r\n\r\n")
+    n, done, err = headers.Parse(data)
+    require.NoError(t, err)
+    require.NotNil(t, headers)
+    assert.Equal(t, "localhost   :   42069", headers["Host"])
+    assert.Equal(t, 29, n)
+    assert.False(t, done)
 }
