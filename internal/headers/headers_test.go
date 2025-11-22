448942379f7808ccd9de54a44d4fb9cc6a60110f
Brian Ott <bott@b1-systems.de>
2025-11-22 18:17:42 +0100

    update: lowercase key

--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -14,7 +14,7 @@ func TestHeadersParse(t *testing.T) {
     n, done, err := headers.Parse(data)
     require.NoError(t, err)
     require.NotNil(t, headers)
-    assert.Equal(t, "localhost:42069", headers["Host"])
+    assert.Equal(t, "localhost:42069", headers["host"])
     assert.Equal(t, 23, n)
     assert.False(t, done)
 
@@ -24,7 +24,7 @@ func TestHeadersParse(t *testing.T) {
     n, done, err = headers.Parse(data)
     require.NoError(t, err)
     require.NotNil(t, headers)
-    assert.Equal(t, "localhost:42069", headers["Host"])
+    assert.Equal(t, "localhost:42069", headers["host"])
     assert.Equal(t, 57, n)
     assert.False(t, done)
 
@@ -34,8 +34,8 @@ func TestHeadersParse(t *testing.T) {
     n, done, err = headers.Parse(data)
     require.NoError(t, err)
     require.NotNil(t, headers)
-    assert.Equal(t, "localhost:42069", headers["Host"])
-    assert.Equal(t, "curl/7.81.0", headers["User-Agent"])
+    assert.Equal(t, "localhost:42069", headers["host"])
+    assert.Equal(t, "curl/7.81.0", headers["user-agent"])
     assert.Equal(t, 25, n)
     assert.False(t, done)
 
